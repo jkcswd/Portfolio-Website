590c4d3b02fb88fd8e7a1e9e7f9ade865296e639
James Spencer <jameskcs1254@gmail.com>
2021-12-29 12:58:03 +0000

    Split profile to skills component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Headline from './components/Headline';
 import Profile from './components/Profile';
 import Projects from './components/Projects';
+import Skills from './components/Skills';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Headline/>
           <Profile/>
         </article>
+        <Skills/>
         <Projects/>
         <Footer/>
       </section>
