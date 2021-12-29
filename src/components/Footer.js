f4b022759fcc196cd5ef64d16a6ce26fa2dee8e6
James Spencer <jameskcs1254@gmail.com>
2021-12-29 15:23:45 +0000

    Add styling and markup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import '../styles/Footer.css';
 const Footer = () => {
   return (
     <footer className="Footer">
+      <p>Created using React, <a href="https://github.com/jkcswd/Portfolio-Website">view source code</a>.</p>
     </footer>
   );
 }
