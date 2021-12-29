96ad416cff83968ce7935e4ee8fc167045d38af6
James Spencer <jameskcs1254@gmail.com>
2021-12-29 13:11:34 +0000

    add Aos fade in  animations

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -5,18 +6,29 @@ import Headline from './components/Headline';
 import Profile from './components/Profile';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
+import Aos from 'aos';
+import 'aos/dist/aos.css'
 
 function App() {
+  useEffect(() => {
+    Aos.init({ duration: 3000 })
+  }, [])
+
   return (
     <div className="App">
       <Header/>
       <section className="content">
-        <article className="main-content">
+        <article className="main-content" data-aos="fade-right">
           <Headline/>
           <Profile/>
         </article>
-        <Skills/>
-        <Projects/>
+        <div data-aos="fade-right">
+          <Skills /> 
+        </div>
+        <div data-aos="fade-right">
+         <Projects />
+        </div>
+        
         <Footer/>
       </section>
     </div>
