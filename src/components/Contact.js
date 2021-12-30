5f2c252abad9de6e8de1dff6567e4f183cc9d9eb
James Spencer <jameskcs1254@gmail.com>
2021-12-30 16:05:20 +0000

    create contact component, styling and markup

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import '../styles/Contact.css';
+
+const Contact = () => {
+  return (
+    <section className="Contact">
+      <h1>Contact</h1>
+      <p>Contact me at: jameskcspencer@gmail.com</p>
+    </section>
+  );
+}
+
+export default Contact;
