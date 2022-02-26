098c0e6964d75508c669c0593ebb212f03f18941
James Spencer <jameskcs1254@gmail.com>
2022-02-26 14:15:51 +0000

    Replace weather app project for blog

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,6 +12,15 @@ const Projects = () => {
   return (
     <section className="Projects">
       <h1>Projects</h1>
+      <Project
+        imageUrl="/images/Blog.png" 
+        imageAlt="Screenshot of Blog" 
+        info="
+          An example E-commerce Website. It consumes an example API from the Fake Store API, takes the data and displays it in
+          a functional React App which uses React-Router-Dom and dynamic client-side routing. I increased my confidence using
+          React-Router-Dom, asynchronous JavaScript in React and how the unidirectional ﬂow of data works.
+          "
+      />
       <Project 
         imageUrl="/images/FakeShop.png" 
         imageAlt="Screenshot of e-commerce app" 
@@ -28,15 +37,6 @@ const Projects = () => {
           A single player Battleship game against a computer.I created this app to learn TDD and in particular unit testing. I built it
           using JavaScript, Jest, HTML, Scss/Sass, NPM, Webpack, Babel and ESLint. I was able to further my knowledge of TDD,
           unit testing and Scss/Sass by doing this.
-          "
-      />
-      <Project
-        imageUrl="/images/Weather.png" 
-        imageAlt="Screenshot of weather app" 
-        info="
-          A web app for looking up the current weather conditions for a location. I constructed the app using ES8 JavaScript
-          (async/await), OpenWeather API, HTML/CSS, NPM, Webpack and Babel. From this I was able to solidify my knowledge of
-          asynchronous JavaScript, the JavaScript event loop, APIs and JSON data.
           "
       />
       <p>Find all my projects on my <a href="https://github.com/jkcswd">github</a> </p>
